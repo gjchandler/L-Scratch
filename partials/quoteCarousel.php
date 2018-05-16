397fe93645541bdbb181317815ce9dae7bf4f139
haa-gg <glendon.gengel@outlook.com>
2018-05-16 14:43:28 -0500

    Now with more animation!

--- partials/quoteCarousel.php
+++ partials/quoteCarousel.php
@@ -2,107 +2,111 @@
 
 <div class="bg-gray">
 
-<div class="quote-carousel-outer">
+	<div class="quote-carousel-outer">
 
-<div class="theme-bg text-white text-center">
+		<div class="theme-bg text-white text-center">
 
-	<div class="container">
+			<div class="container">
 
-		<div class="row">
+				<div class="row">
 
-			<div class="col-sm-12">
+					<div class="col-sm-12">
 
-				<div id="carouselExampleIndicators" class="quote-carousel carousel slide" data-ride="carousel">
+						<div id="carouselExampleIndicators" class="quote-carousel carousel slide" data-ride="carousel">
 
-					<ol class="carousel-indicators">
+							<ol class="carousel-indicators">
 
-						<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+								<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
 
-						<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+								<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
 
-						<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+								<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
 
-					</ol>
+							</ol>
 
-					<div class="carousel-inner">
+							<div class="carousel-inner">
 
-						<div class="carousel-item active">
+								<div class="carousel-item active">
 
-							<div class="quote">
+									<div class="quote">
 
-								<div class="quote-inner">
+										<div class="quote-inner">
 
-									<p>
-										"I love floors, they never let you down.  They are so underappreciated.  Not as much as <span class="theme-color">tunnels</span>, though.  Just try one."
-									</p>
+											<p>
+												"<span class="theme-color">Our business</span> was founded to evolve the status quo of the life insurance industry with innovative products and services."
+											</p>
+
+										</div>
+
+										<div class="byline">
+											- Jon Sabes, CEO
+										</div>
+
+									</div>
 
 								</div>
 
-								<div class="byline">
-									Elon Musk, Tesla CEO
+								<div class="carousel-item">
+
+									<div class="quote">
+
+										<div class="quote-inner">
+
+											<p>
+												“I believe in this product so much, I tattooed the <span class="theme-color">Life Epigenetics</span> logo on my back”
+											</p>
+
+										</div>
+
+										<div class="byline">
+											- Dr. Chen (probably)
+										</div>
+
+									</div>
+
+								</div>
+
+								<div class="carousel-item">
+
+									<div class="quote">
+
+										<div class="quote-inner">
+
+											<p>
+												"I love floors, they never let you down.  They are so underappreciated.  Not as much as <span class="theme-color">tunnels</span>, though.  Just try one."
+											</p>
+
+										</div>
+
+										<div class="byline">
+											- Elon Musk, Tesla CEO
+										</div>
+
+									</div>
+
 								</div>
 
 							</div>
 
-						</div>
+							<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
 
-						<div class="carousel-item">
+								<span class="carousel-control-prev-icon" aria-hidden="true"></span>
 
-							<div class="quote">
+								<span class="sr-only">Previous</span>
 
-								<div class="quote-inner">
+							</a>
 
-									<p>
-										"I love floors, they never let you down.  They are so underappreciated.  Not as much as <span class="theme-color">tunnels</span>, though.  Just try one."
-									</p>
+							<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
 
-								</div>
+								<span class="carousel-control-next-icon" aria-hidden="true"></span>
 
-								<div class="byline">
-									Elon Musk, Tesla CEO
-								</div>
+								<span class="sr-only">Next</span>
 
-							</div>
-
-						</div>
-
-						<div class="carousel-item">
-
-							<div class="quote">
-
-								<div class="quote-inner">
-
-									<p>
-										"I love floors, they never let you down.  They are so underappreciated.  Not as much as <span class="theme-color">tunnels</span>, though.  Just try one."
-									</p>
-
-								</div>
-
-								<div class="byline">
-									Elon Musk, Tesla CEO
-								</div>
-
-							</div>
+							</a>
 
 						</div>
 
 					</div>
-
-					<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-
-						<span class="carousel-control-prev-icon" aria-hidden="true"></span>
-
-						<span class="sr-only">Previous</span>
-
-					</a>
-
-					<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-
-						<span class="carousel-control-next-icon" aria-hidden="true"></span>
-
-						<span class="sr-only">Next</span>
-
-					</a>
 
 				</div>
 
@@ -111,10 +115,6 @@
 		</div>
 
 	</div>
-
-</div>
-
-</div>
 
 </div>
 
