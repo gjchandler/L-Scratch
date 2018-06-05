bd544fbd5080b265132c7313182d024ab546c8a3
Grant Chandler <gchandler@macpro-jpalm.gwgoflca.com>
2018-06-05 10:44:53 -0500

    testing out practical 2 page

---
+++ practial2.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<!--[if lte IE 6]><html class="preIE7 preIE8 preIE9"><![endif]-->
+<!--[if IE 7]><html class="preIE8 preIE9"><![endif]-->
+<!--[if IE 8]><html class="preIE9"><![endif]-->
+<!--[if gte IE 9]><!--><html><!--<![endif]-->
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <meta name="viewport" content="width=device-width,initial-scale=1">
+  <title>title</title>
+
+  <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/bootstrap.css" type="text/css">
+  <link rel="stylesheet" href="css/styles.css" type="text/css">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.min.js"></script>
+</head>
+<body>
+
+  <?php  
+  include 'partials/topNavSticky.php'; 
+  include 'partials/quoteBar.php'; 
+  include 'partials/ctaBlue.php'; 
+  include 'partials/overpay.php';
+  include 'partials/mission.php';
+  include 'partials/rateTable.php';
+  include 'partials/imgFullWidth.php';
+  include 'partials/evolution.php';
+  include 'partials/services.php';
+  include 'partials/qualifyChart.php';
+  include 'partials/team.php';
+  include 'partials/quoteCta.php';
+  include 'partials/footer.php';
+  ?>
+
+  <script src="js/jquery-3.1.1.slim.min.js"></script>
+  <script src="js/bootstrap.js"></script>
+  <script src="js/custom.js"></script>
+
+</body>
+</html>
